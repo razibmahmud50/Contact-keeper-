efec97ba2ee0b004536fd27f42b7097c8c2f1297
razibmahmud50 <razibmahmud50@gmail.com>
2020-05-17 17:01:54 +0600

    initialize project

---
+++ routes/contacts.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+
+//@route  GET  api/contacts @desc Get all users contact @access Private
+router.get('/', (req, res)=>{
+    res.send('get all contact')
+})
+
+//@route   POST  api/contacts @desc Add new contact @access Private
+router.post('/', (req, res)=>{
+    res.send('Add new contanct')
+})
+
+//@route   PUt  api/contacts/:id @desc update contact @access Private
+router.put('/:id', (req, res)=>{
+    res.send('update contact')
+})
+
+//@route   GET  api/contacts @desc Get all users contact @access  Private
+router.delete('/:id', (req, res)=>{
+    res.send('delete contact')
+})
+
+module.export = router
