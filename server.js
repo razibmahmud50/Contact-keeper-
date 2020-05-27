5938000a5f4162234138895ab9dc5bd052ceecf7
razibmahmud50 <razibmahmud50@gmail.com>
2020-05-27 14:54:33 +0600

    middleware updated

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express()
+
+//connect db
+connectDB()
+
+//init middleware
+
+app.use(express.json({extended: false}))
 
 
 app.get('/',(req, res)=>res.send({msg: "welcome to our contact keeper api "}))
@@ -11,6 +19,6 @@ app.use('/api/contacts', require('./routes/contacts'))
 
 
 
-const PORT = process.env.PORT || 2000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, ()=>console.log(`Server started on port ${PORT}`))
